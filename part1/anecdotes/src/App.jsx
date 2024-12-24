bf22e8c0d7ab468d79362285ddd8034e1fd4a923
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2024-12-24 15:10:07 +0800

    step 2 and step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from "react"
 
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const MostVotes = ({ anecdotes, points }) => {
+
+  const sumOfPoints = Object.values(points).reduce((acc, value) => acc + value, 0)
+
+  if (sumOfPoints !== 0) {
+
+    const highestScorer = Object.entries(points).reduce((max, [index, score]) => {
+      return score > max.score ? { index, score } : max;
+    }, { index: '', score: -Infinity });
+
+    console.log(`highestScorer index: ${highestScorer.index}`)
+    console.log(`highestScorer score: ${highestScorer.score}`)
+
+    return (
+      <>
+        <h1>Anecdote with most votes</h1>
+        <div>{anecdotes[highestScorer.index]}</div>
+        <div>has {highestScorer.score} votes</div>
+      </>
+    )
+  }
+
+}
+
 
 const App = () => {
 
   const anecdotes = [
     'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes i t later!',
+    'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
@@ -15,20 +45,47 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0
+  })
+
+  console.log('points: ', points)
+
+  const maxNumber = anecdotes.length
 
   const randomIndex = () => {
-    const maxNumber = anecdotes.length;
     const randomNumber = Math.floor(Math.random() * (maxNumber - 0) + 0);
-    setSelected(randomNumber);
-    console.log(randomNumber);
+    setSelected(randomNumber)
+    console.log('showing', randomNumber)
   }
+
+  const handleVote = () => {
+    console.log('vote pressed for', selected);
+    const newVote = {
+      ...points,
+      [selected]: points[selected] + 1
+    }
+    setPoints(newVote)
+  }
+
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
       <div>
-        <button onClick={randomIndex}>next anecdote</button>
+        <Button handleClick={handleVote} text='vote' />
+        <Button handleClick={randomIndex} text='next anecdote' />
       </div>
+      <MostVotes anecdotes={anecdotes} points={points} />
     </div>
   )
 }
