48dc06715a71f07e0ca7f876df07bbffefd16ea5
Gilbert <21050167+GilbertMarasigan@users.noreply.github.com>
2024-12-31 16:39:09 +0800

    step 4 search feature

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '123-456-789'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const addPerson = (event) => {
 
@@ -21,7 +23,7 @@ const App = () => {
 
     // check for duplicates
     const countMatchingPerson = persons.filter(person => person.name === newName).length
-    countMatchingPerson === 1 ? alert(`${newName} is already added to phonebook`) :     setPersons(persons.concat(newNameObject))
+    countMatchingPerson === 1 ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(newNameObject))
 
   }
 
@@ -29,17 +31,33 @@ const App = () => {
     event.target.id === 'name' ? setNewName(event.target.value) : setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase()
+    setSearch(searchTerm)
+  }
 
+  const showNumbers = () => {
+    
+    let numbersObject = persons
+
+    if (search !== '') {
+      numbersObject = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+    }
+  
+    return numbersObject.map((person) => <div key={person.name}>{person.name} {person.number}</div>)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input type='text' id='input-search' onChange={handleSearch} /></div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input id='name' onChange={handleInputChange} />
         </div>
         <div>
-          number: <input id='number' type="text" onChange={handleInputChange}/>
+          number: <input id='number' type="text" onChange={handleInputChange} />
         </div>
         <div>
           <button type='submit'>add</button>
@@ -48,7 +66,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+        {showNumbers()}
       </div>
     </div>
   )
